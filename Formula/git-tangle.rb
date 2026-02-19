89f4204eb16dba7a585b2370626ab477d55afcdb
Taygun Savaş <taygunsavas@windowslive.com>
2026-02-19 21:23:24 +0300

    git-tangle 1.0.0 (#6)
    Co-authored-by: tangle-bot <tangle-bot@users.noreply.github.com>

--- Formula/git-tangle.rb
+++ Formula/git-tangle.rb
@@ -1,8 +1,8 @@
 class GitTangle < Formula
   desc "A branching workflow tool that untangles your repo"
   homepage "https://github.com/taygunsavas/git-tangle"
-  url "https://github.com/taygunsavas/git-tangle/releases/download/0.1.6/git-tangle_0.1.6.tar.gz"
-  sha256 "1c7129471fe999a1b0c933b667d1ea37909709898242b0beb2d9556b334cc5e8"
+  url "https://github.com/taygunsavas/git-tangle/releases/download/1.0.0/git-tangle_1.0.0.tar.gz"
+  sha256 "007155227f2fa10db0ee10667cfdc3b335a26322ac51b3b5afe1456eba78fb9b"
   license "MIT"
 
   def install
